01e8a3a0cb4d33b2285cb5bce932995a3e45734b
Tarcisio Moreira <tmoreirs@everis.com>
2021-04-06 16:57:16 -0300

    Fazendo um maven build

--- src/main/java/com/pro/projetoh2/resources/ClienteControler.java
+++ src/main/java/com/pro/projetoh2/resources/ClienteControler.java
@@ -3,11 +3,13 @@ package com.pro.projetoh2.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pro.projetoh2.models.Cliente;
@@ -36,6 +38,7 @@ public class ClienteControler {
 	}
 	
 	@PostMapping("/cliente")
+	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente addCliente(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
@@ -44,4 +47,5 @@ public class ClienteControler {
 	public Cliente buscaId(@PathVariable long id) {
 		 return clienteRepository.findById(id);
 	}
+	
 }
